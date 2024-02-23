d23a4df95c515e91b0086113a4c183905c55b1e7
Qobil6221 <sh.yuldashbekov@gmail.com>
2024-02-23 13:41:59 +0500

    emailga habar yuborish

---
+++ apps/books/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import BookListView, BookDetailView, AddReviewView
+
+app_name = "books"
+
+urlpatterns = [
+    path('', BookListView.as_view(), name="book-list"),
+    path('<slug:slug>/', BookDetailView.as_view(), name="book-detail"),
+    path('<int:pk>', AddReviewView.as_view(), name="add-review")
+
+]
